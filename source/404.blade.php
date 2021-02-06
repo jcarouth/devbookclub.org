5c5e5463d8fbaa2b5bdefc268e80c3534a06675f
Jeff Carouth <jcarouth@gmail.com>
2021-02-06 14:02:37 -0600

    Configure netlify for deployment

---
+++ source/404.blade.php
@@ -0,0 +1,14 @@
+---
+permalink: 404.html
+---
+@extends('_layouts.master')
+
+@section('body')
+    <div class="container mx-auto h-full pt-16 md:pt-32">
+        <h1 class="text-5xl text-center">Page not found.</h1>
+
+        <p class="mt-10">
+            This is unfortunate. The page you wanted to see does not exist. Our sincerest apologies.
+        </p>
+    </div>
+@endsection
