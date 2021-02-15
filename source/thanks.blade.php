67f50e88fe7eded914767979fe228300d7dbe273
Jeff Carouth <jcarouth@gmail.com>
2021-02-15 14:24:39 -0600

    MVP join form for Refactoring

---
+++ source/thanks.blade.php
@@ -0,0 +1,13 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="container mx-auto px-3">
+    <h1 class="mt-8 text-4xl border-b-2 border-gray-300">Thanks!</h1>
+
+    <img class="mt-8 mx-auto w-80 lg:w-full" src="/assets/images/never-stop-learning-hero.png" alt="Motivational quote Never stop learning" width="1280" height="720" loading="lazy">
+
+    <p class="mt-8 mx-auto max-w-3xl">
+        Thank you for expressing interest in joining the discussion! We hope to talk with you soon!
+    </p>
+</div>
+@endsection
