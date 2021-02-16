047fefabbd222d3905d3235d19896efa66401628
Jeff Carouth <jcarouth@gmail.com>
2021-02-16 00:17:49 -0600

    Add Titles to all pages

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 return [
     'production' => false,
     'baseUrl' => 'http://localhost:8000',
-    'title' => 'DevBookClub',
+    'siteName' => 'Dev Book Club',
     'description' => 'As developers we need to constantly learn and grow. The Dev Book Club is a chance to read through and discuss what you are learning with fellow devs of all skill levels and backgrounds.',
     'collections' => [],
 ];
