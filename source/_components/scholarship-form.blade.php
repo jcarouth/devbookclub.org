5045b2d8d1d0a84b0c6766205fe573ce5e62d02d
Jeff Carouth <jcarouth@gmail.com>
2021-02-20 15:47:57 -0600

    Add scholarship page

---
+++ source/_components/scholarship-form.blade.php
@@ -0,0 +1,34 @@
+<div class="my-20">
+    <div class="pb-2 text-2xl border-b-2 border-gray-100">Apply for the scholarship</div>
+
+    <form name="scholarship" method="post" data-netlify="true" class="block mt-10 space-y-6">
+        <div>
+            <label for="scholarship-app-name" class="block">Name *</label>
+            <input id="scholarship-app-name" class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="text" name="name" required="required">
+        </div>
+
+        <div>
+            <label for="scholarship-app-email" class="block">Email *</label>
+            <input id="scholarship-app-email" class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="email" name="email" required="required">
+        </div>
+
+        <div>
+            <label for="scholarship-app-twitter" class="block">Twitter</label>
+            <input id="scholarship-app-twitter" class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="text" name="twitter">
+        </div>
+
+        <div>
+            <label for="scholarship-app-acquisition" class="block">How did you hear about the Dev Book Club?</label>
+            <textarea id="scholarship-app-acquisition" class="block w-full mt-1 px-2 py-1 border border-gray-900" name="acquisition"></textarea>
+        </div>
+
+        <div>
+            <label for="scholarship-app-justification" class="block">Why are you interested in reading this book?</label>
+            <textarea id="scholarship-app-justification" class="block w-full mt-1 px-2 py-1 border border-gray-900" name="justification"></textarea>
+        </div>
+
+        <div>
+            <button class="block mr-auto mt-8 px-10 py-1 rounded-2xl bg-gray-500 text-lg text-white uppercase" type="submit">Apply</button>
+        </div>
+    </form>
+</div>
---
+++ source/_pages/scholarship.blade.md
@@ -0,0 +1,32 @@
+---
+extends: _layouts.page
+title: Dev Book Club Scholarship
+slug: scholarship
+section: page-content
+---
+At the outset of the Dev Book Club, the goal was clear: to make learning through reading technical books accessible and give developers an outlet where they can talk about challenging material and their experiences with applying what they are learning about. That is the reason the discussions are archived on a YouTube channel: to provide a resource for other people long after the initial discussion is over.
+
+The key word in the preceding paragraph is accessible. When Jeff started Dev Book Club he wanted to facilitate a forum for anyone of any background, level of experience, or means to be able to benefit from the book "Implementing Domain-Driven Design." To that end, Jeff is going to sponsor one participant for each book discussed by this group.
+
+@include('_components.scholarship-form')
+
+## FAQ
+
+### Who is eligible for this scholarship?
+
+Anyone who is interested in reading the book is eligible. Ideally you are from a traditionally-underrepresented group in Tech and are either just getting started or looking to enter the ranks of more senior level developers, but this is not a requirement.
+
+### What is included?
+
+You will receive a copy of the book (or the means with which to purchase the book yourself) the club is currently discussing.
+
+### Is there anything I have to do?
+
+Nothing is required of you. It would be nice if you were able to participate in the discussion groups for the book, but it is understandable if you do not wish to. You will still have access to streamed discussions.
+
+### How do I apply?
+
+All you have to do is submit the application. A recipient will be selected and contacted with follow up details.
+
+
+
