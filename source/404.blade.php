8e074b71ff92e3aca338a5c62ad90c40dccdb14f
Jeff Carouth <jcarouth@gmail.com>
2021-02-20 11:17:53 -0600

    Reformat forms ditching centered/restricted look

--- source/404.blade.php
+++ source/404.blade.php
@@ -6,7 +6,7 @@ title: Not found
 
 @section('body')
     <div class="container mx-auto px-3">
-        <h1 class="mt-8 text-4xl border-b-2 border-gray-300">Page not found</h1>
+        <h1 class="mt-8 pb-3 text-4xl border-b-2 border-gray-300">Page not found</h1>
 
         <p class="mt-10">
             This is unfortunate. The page you wanted to see does not exist. Our sincerest apologies.
