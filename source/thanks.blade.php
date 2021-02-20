0c6686f72ace17e000c9cac9aedd85ac422e92bf
Jeff Carouth <jcarouth@gmail.com>
2021-02-20 11:29:20 -0600

    Apply style updates to /thanks/

--- source/thanks.blade.php
+++ source/thanks.blade.php
@@ -5,17 +5,14 @@ title: Thank you!
 
 @section('body')
 <div class="container mx-auto px-3">
-    <h1 class="mt-8 text-4xl border-b-2 border-gray-300">Thanks!</h1>
+    <h1 class="mt-8 pb-3 text-4xl border-b-2 border-gray-300">Thanks!</h1>
 
-    <div class="flex flex-col lg:flex-row lg:space-x-4">
-        <div class="w-full lg:w-1/2">
-            <img class="mt-8 mx-auto w-80 lg:w-full" src="/assets/images/never-stop-learning-hero.png" alt="Motivational quote Never stop learning" width="1280" height="720" loading="lazy">
-        </div>
-        <div class="mt-8 w-full lg:w-1/2">
-            <p class="font-bold">
-                Thank you for expressing interest in joining the discussion! We hope to talk with you soon!
-            </p>
-        </div>
-    </div>
+    <p class="max-2-3xl mt-10 text-xl">
+        Thank you for expressing interest in joining the discussion! We hope to talk with you soon!
+    </p>
+
+    <p class="max-2-3xl mt-10 text-xl">
+        <a href="/" class="underline">Head back to the homepage.</a>
+    </p>
 </div>
 @endsection
