a80c814b6eef3e86c3144195378870935449cd75
Jeff Carouth <jcarouth@gmail.com>
2021-02-20 11:17:53 -0600

    Reformat forms ditching centered/restricted look

--- source/join.blade.php
+++ source/join.blade.php
@@ -5,32 +5,28 @@ title: Join the discussion
 
 @section('body')
 <div class="container mx-auto px-3">
-    <h1 class="mt-8 text-4xl border-b-2 border-gray-300">Join the Discussion</h1>
+    <h1 class="mt-8 pb-3 text-4xl border-b-2 border-gray-300">Join the Discussion</h1>
 
-    <div class="max-w-3xl mx-auto">
-        <div class="mt-8">
-            <p class="">
-                The Dev Book Club is currently planning to discuss the book, Refactoring: Improving the Design of Existing Code by Martin Fowler. To join the conversation, fill out the form below and an organizer will contact you about the next scheduled discussion time.
-            </p>
+    <p class="max-w-3xl mt-8 text-xl">
+        The Dev Book Club is currently planning to discuss the book, Refactoring: Improving the Design of Existing Code by Martin Fowler. To join the conversation, fill out the form below and an organizer will contact you about the next scheduled discussion time.
+    </p>
 
-            <div class="bg-gray-600 mt-8 mx-auto p-8 lg:rounded-2xl text-white">
-                <form data-netlify="true" name="join-refactoring" action="/thanks" method="post">
-                    <label class="block"><strong>Name *</strong>
-                        <input class="block w-full border-b-2 border-white bg-gray-600 mt-2 p-2" type="text" name="name" required>
-                    </label>
+    <div class="mt-10">
+        <form data-netlify="true" name="join-refactoring" action="/thanks" method="post">
+            <label class="block"><strong>Name *</strong>
+                <input class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="text" name="name" required>
+            </label>
 
-                    <label class="block mt-4"><strong>Email *</strong>
-                        <input class="block w-full border-b-2 border-white bg-gray-600 mt-2 p-2" type="email" name="email" required>
-                    </label>
+            <label class="block mt-8"><strong>Email *</strong>
+                <input class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="email" name="email" required>
+            </label>
 
-                    <label class="block mt-4"><strong>Twitter Handle</strong>
-                        <input class="block w-full border-b-2 border-white bg-gray-600 mt-2 p-2" type="text" name="twitter">
-                    </label>
+            <label class="block mt-8"><strong>Twitter Handle</strong>
+                <input class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="text" name="twitter">
+            </label>
 
-                    <button class="block w-1/2 mx-auto mt-8 border-2 border-white rounded-md py-3 px-4 font-bold uppercase" type="submit">Submit</button>
-                </form>
-            </div>
-        </div>
+            <button class="block mr-auto mt-8 px-10 py-1 rounded-2xl bg-gray-500 text-lg text-white uppercase" type="submit">Submit</button>
+        </form>
     </div>
 </div>
 @endsection
--- source/meet-and-greet.blade.php
+++ source/meet-and-greet.blade.php
@@ -1,48 +1,51 @@
 ---
-title: Meet & Greet
+title: RSVP for DevBookClub Meet & Greet
 ---
 @extends('_layouts.master')
 
 @section('body')
 <div class="container mx-auto px-3">
-    <h1 class="mt-8 text-4xl border-b-2 border-gray-300">Meet & Greet</h1>
+    <h1 class="mt-8 pb-3 text-4xl border-b-2 border-gray-300">RSVP - Meet & Greet for Refactoring Discussion Group</h1>
 
-    <p class="max-w-3xl mt-8 mx-auto text-xl">
-        Before we kick off the discussions let's get to know each other. Pick one (or more) of the scheduled meet and greets to attend. These will be virtual (of course) via Zoom.
+    <p class="max-w-3xl mt-10 text-xl">
+        The discussion groups for Refactoring, Improving the Design of Existing Code by Martin Fowler is going to begin soon. Because of the nature of how these discussions work -- virtual and streaming/archived -- it is imperative we are able to meet with limited technical issues. To participate you will need to ensure you are able to connect via Zoom to the group and have your audio under control. To facilitate figuring this all out, we will have a few "Meet & Greet" Zoom meetings available. If yo want to participate and would like an opportunity to meet other participants, RSVP to one or more of the meetings below.
     </p>
-</div>
 
+    <p class="max-w-3xl mt-10 text-xl">
+        Don't forget to sign up for the mailing list (and <a class="underline" href="https://twitter.com/_devbookclub" aria-label="">follow @_devbookclub on Twitter</a>) to get notifications for upcoming discussion groups.
+    </p>
 
-<div x-data="meetandgreets()" class="bg-gray-600 max-w-3xl mt-8 mx-auto p-8 lg:rounded-2xl text-white">
-    <div class="text-3xl border-b-2 border-gray-300">RSVP</div>
-    <form x-ref="meetandgreetsForm" name="meetandgreets-rsvps" data-netlify="true" method="post" action="/thanks">
-        <div class="max-w-3xl mx-auto space-y-3">
-            <template x-for="event in events">
-            <div @click="toggleEvent(event.id)" class="flex items-center">
-                <div class="w-8 h-8 mr-3 border-2">
-                    <svg x-show="eventSelected(event.id)" class="w-6 h-6 mx-auto fill-current"><use xlink:href="/assets/build/icons/spritemap.svg#sprite-check"></use></svg>
+    <div x-data="meetandgreets()" class="mt-10">
+        <form x-ref="meetandgreetsForm" name="meetandgreets-rsvps" data-netlify="true" method="post" action="/thanks">
+            <label>Name: *</label>
+            <input x-model="name" class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="text" name="name" required>
+
+            <label class="block mt-8">Email: *</label>
+            <input x-model="email" class="block w-full lg:w-1/2 mt-1 px-2 py-1 border border-gray-900" type="email" name="email" required>
+
+            <label class="block mt-8">Select at least 1 date/time:</label>
+            <div class="space-y-4">
+                <template x-for="event in events">
+                <div @click="toggleEvent(event.id)" class="flex items-center">
+                    <div class="w-8 h-8 mr-3 border-2">
+                        <svg x-show="eventSelected(event.id)" class="w-6 h-6 mx-auto fill-current"><use xlink:href="/assets/build/icons/spritemap.svg#sprite-check"></use></svg>
+                    </div>
+                    <div class="">
+                        <div class="font-bold text-2xl" x-text="event.title"></div>
+                        <span class="block text-sm" x-text="event.time"></span>
+                    </div>
                 </div>
-                <div class="">
-                    <div class="font-bold text-2xl" x-text="event.title"></div>
-                    <span class="block text-sm" x-text="event.time"></span>
-                </div>
+                </template>
+                <input x-model="selectedEvents" type="hidden" name="events">
             </div>
-            </template>
-        </div>
 
-        <div class="max-w-3xl mt-8 mx-auto">
-            <label>Name:</label>
-            <input x-model="name" class="block w-full lg:w-1/2 mt-1 px-2 py-1 text-gray-800" type="text" name="name">
-            <label class="block mt-3">Email:</label>
-            <input x-model="email" class="block w-full lg:w-1/2 mt-1 px-2 py-1 text-gray-800" type="email" name="email">
-            <input x-model="selectedEvents" type="hidden" name="events">
-            <input type="hidden" name="form-name" value="meetandgreets-rsvps" />
-            <div x-cloak x-show="resultText != ''" x-text="resultText" :class="{'text-red-500': error}" class="mt-8"></div>
-            <button @click.prevent="submit()" class="block border border-white mr-auto mt-8 px-10 py-1 rounded-2xl">
-                <span>Sign Up</span>
-            </button>
-        </div>
-    </form>
+            <div class="mt-8">
+                <div x-cloak x-show="resultText != ''" x-text="resultText" :class="{'text-red-500': error}" class="mt-8"></div>
+                <button @click.prevent="submit()" class="block mr-auto mt-8 px-10 py-1 rounded-2xl bg-gray-500 text-lg text-white">
+                    <span>Sign Me Up</span>
+                </button>
+            </div>
+        </form>
+    </div>
 </div>
-
 @endsection
