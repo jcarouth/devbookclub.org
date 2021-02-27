89eb3a7852dc048331d1b16816702976b69ad533
Jeff Carouth <jcarouth@gmail.com>
2021-02-26 19:48:14 -0600

    Adjust homepage design

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,31 +4,21 @@
 <h1 class="hidden">Dev Book Club</h1>
 
 <div class="max-w-7xl mx-auto">
-    <img class="mx-auto w-80 lg:w-full" src="/assets/images/never-stop-learning-hero.png" alt="Motivational quote Never stop learning" width="1280" height="720" loading="lazy">
-</div>
-
-<div class="max-w-7xl mx-auto mt-10 lg:mt-24 lg:flex lg:flex-row lg:space-x-4">
-    <div class="lg:w-1/2 px-3 py-6 text-center">
-        <h2 class="text-3xl uppercase font-bold">Rebooting...</h2>
-
-        <p class="mt-8 mx-auto max-w-3xl">
-            We are a group of developers around the world gaining knowledge and sharing experiences by reading a book.
-        </p>
-
-        <div class="w-5/6 mt-8 mx-auto border-2 border-gray-900 font-bold px-3 py-1 rounded-2xl text-xl">
-            <a href="/join/">Join the Conversation</a>
+    <div class="mt-10 lg:flex lg:flex-row lg:shadow-m">
+        <div class="lg:w-1/2 mt-10 lg:mt-0">
+            <img class="w-full h-full object-cover" src="/assets/images/never-stop-learning-hero.png" alt="Motivational quote Never stop learning" width="1280" height="720" loading="lazy">
         </div>
-    </div>
-    <div class="lg:w-1/2 mt-10 lg:mt-0 px-3 py-6 rounded-2xl bg-gray-600 text-white text-center">
-        <h2 class="text-3xl uppercase">Meet and Greets</h2>
+        <div class="lg:w-1/2 mt-10 lg:mt-0 px-6 py-12 bg-gray-600 text-white">
+            <h2 class="text-3xl lg:text-4xl uppercase tracking-wider">Meet and Greets</h2>
 
-        <p class="mt-8 mx-auto text-left">
-            Before we kick off the next discussion of Refactoring by Martin Fowler, there will be a series of Meet and Greets
-            for the group to settle on schedules and work out the kinks of the technology.
-        </p>
+            <p class="mt-4 mx-auto text-left">
+                Before we kick off the next discussion of Refactoring by Martin Fowler, there will be a series of Meet and Greets
+                for the group to settle on schedules and work out the kinks of the technology.
+            </p>
 
-        <div class="w-5/6 mt-8 mx-auto border-2 border-white font-bold px-3 py-1 rounded-2xl text-xl">
-            <a href="/rsvp/">RSVP to an upcoming Meet & Greet</a>
+            <div class="inline-flex mt-8 mx-auto px-5 py-3 rounded-md bg-white text-gray-900 text-xl font-bold  ">
+                <a href="/rsvp/">RSVP to an upcoming Meet & Greet</a>
+            </div>
         </div>
     </div>
 </div>
