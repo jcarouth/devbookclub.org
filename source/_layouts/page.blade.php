f2b683bc7f3becfc8c1968fd8103d70b3d0ed543
Jeff Carouth <jcarouth@gmail.com>
2021-02-28 11:58:31 -0600

    Massive design update

--- source/_layouts/page.blade.php
+++ source/_layouts/page.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('body')
-<div class="md-content">
+<div class="md-content max-w-6xl mx-auto px-3">
     <h1 class="mt-8 pb-3 text-4xl border-b-2 border-gray-300">{{ $page->title }}</h1>
 
     <div class="max-w-3xl mt-10">
