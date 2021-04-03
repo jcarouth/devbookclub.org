91882431b212054b93e26f7f3c60a6bd6f98d5f0
Jeff Carouth <jcarouth@gmail.com>
2021-04-02 23:57:22 -0500

    Reword RSVP blurb

--- source/rsvp-uk.blade.php
+++ source/rsvp-uk.blade.php
@@ -9,7 +9,7 @@ description: RSVP for an upcoming DevBookClub meeting or discussion.
     <h1 class="mt-8 pb-3 text-4xl border-b-2 border-gray-300">RSVP - Refactoring - UK/EU Group</h1>
 
     <p class="max-w-3xl mt-10 text-xl">
-        Discussions covering Refactoring: Improving the Design of Existing Code by Martin Fowler are underway. This page is for the group based in the UK. For times that might work better in the US and Canada please see the <a href="/rsvp/">RSVP page for the US-based group</a>.
+        If you would like to join online discussions on Refactoring: Improving the Design of Existing Code by Martin Fowler please use the form below to indicate your interest. For discussion times for North American timezones please see the <a href="/rsvp/">RSVP page for the US-based group</a>.
     </p>
 
     <p class="max-w-3xl mt-10 text-xl">
